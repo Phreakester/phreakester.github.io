3bf65b354d13cd0558bb8aa5b236f84da9e494b6
phreakester <gettyv05@gmail.com>
2021-04-20 15:53:41 -0700

    Made it so gradient is only chosen/changed on page load, not each time the header is rendered

--- src/index.js
+++ src/index.js
@@ -17,9 +17,20 @@ import AboutMe from './components/AboutMe'
 
 import reportWebVitals from './reportWebVitals';
 
-
-
 import project_dict from './assets/projects_data'
+let gradients = [
+  "linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)", 
+  "linear-gradient(90deg, #efd5ff 0%, #515ada 100%)", 
+  "linear-gradient(90deg, #FDBB2D 0%, #22C1C3 100%)",
+  "linear-gradient(90deg, #d53369 0%, #daae51 100%)",
+  "linear-gradient(90deg, #f8ff00 0%, #3ad59f 100%)"
+];
+const gradient = gradients[Math.floor(Math.random() * gradients.length)];
+console.log(gradient);
+var css_body = document.querySelector("body");
+css_body.style.setProperty("--gradient", gradient)
+
+
 
 ReactDOM.render(
   <React.StrictMode>
